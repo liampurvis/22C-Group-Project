95a1befb0df141c4602ddd531487d031a0ecb904
kartik924 <kartik924@gmail.com>
2015-03-13 14:25:18 -0700

    Update BinaryNode.h

--- BinaryNode.h
+++ BinaryNode.h
@@ -1,34 +1,33 @@
-// Node for a binary tree
-// Created by Frank M. Carrano and Tim Henry.
-// Modified by CNguyen
- 
-#ifndef _BINARY_NODE
-#define _BINARY_NODE
+#ifndef Assignment_4_BinaryNode_h
+#define Assignment_4_BinaryNode_h
+
 
 template<class ItemType>
 class BinaryNode
-{   
+{
 private:
-	ItemType              item;         // Data portion
-	BinaryNode<ItemType>* leftPtr;		// Pointer to left child
-	BinaryNode<ItemType>* rightPtr;		// Pointer to right child
-
+    ItemType              item;         // Data portion
+    BinaryNode<ItemType>* leftPtr;		// Pointer to left child
+    BinaryNode<ItemType>* rightPtr;		// Pointer to right child
+    
 public:
-	// constructors
-	BinaryNode(const ItemType & anItem)			   {item = anItem; leftPtr = 0; rightPtr = 0;}
-	BinaryNode(const ItemType & anItem, 
-		       BinaryNode<ItemType>* left, 
-			   BinaryNode<ItemType>* right)		   {item = anItem; leftPtr = left; rightPtr = right;}
-	// accessors
-	void setItem(const ItemType & anItem)		   {item = anItem;}
-	void setLeftPtr(BinaryNode<ItemType>* left)	   {leftPtr = left;}
-	void setRightPtr(BinaryNode<ItemType>* right)  {rightPtr = right;}
-	// mutators
-	ItemType getItem() const					   {return item;}
-	BinaryNode<ItemType>* getLeftPtr() const	   {return leftPtr;}
-	BinaryNode<ItemType>* getRightPtr() const	   {return rightPtr;}
-
-	bool isLeaf() const							   {return (leftPtr == 0 && rightPtr == 0);}
+    // constructors
+    BinaryNode(const ItemType & anItem)			   {item = anItem; leftPtr = 0; rightPtr = 0;}
+    BinaryNode(const ItemType & anItem,
+               BinaryNode<ItemType>* left,
+               BinaryNode<ItemType>* right)		   {item = anItem; leftPtr = left; rightPtr = right;}
+    // accessors
+    void setItem(const ItemType & anItem)		   {item = anItem;}
+    void setLeftPtr(BinaryNode<ItemType>* left)	   {leftPtr = left;}
+    void setRightPtr(BinaryNode<ItemType>* right)  {rightPtr = right;}
+    // mutators
+    ItemType getItem() const					   {return item;}
+    BinaryNode<ItemType>* getLeftPtr() const	   {return leftPtr;}
+    BinaryNode<ItemType>* getRightPtr() const	   {return rightPtr;}
+    
+    bool isLeaf() const							   {return (leftPtr == 0 && rightPtr == 0);}
 }; 
 
-#endif 
+
+
+#endif
