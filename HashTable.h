b4c13ae00f3c94066c8080476a00c0c811f9165e
kartik924 <kartik924@gmail.com>
2015-03-13 16:22:37 -0700

    Update HashTable.h

--- HashTable.h
+++ HashTable.h
@@ -1,20 +1,26 @@
+
+#ifndef Assignment_5_HashTable_h
+#define Assignment_5_HashTable_h
+
 #pragma once
 template <class Object>
 class HashTable
 {
 protected:
-   int (*Hash)(const Object&obj);
-   int (*Compare)(const Object&left, const Object&right);
+    int (*Hash)(const Object&obj);
+    int (*Compare)(const Object&left, const Object&right);
 public:
-   HashTable(int (*hashFcn)(const Object &obj),
-		int (*comp)(const Object&left, const Object&right)){
-			Hash = hashFcn; Compare = comp;}
-   virtual ~HashTable(){}
-   virtual bool contains(const Object & x) const =0;
-   virtual void makeEmpty()=0;
-   virtual bool insert(const Object & x) =0;
-   virtual bool remove(const Object & x) =0;
-   virtual int size() const =0;
-   virtual void displayStatistics() const =0;
-   virtual bool getEntry(const Object & target, Object & returnedItem) const=0;
+    HashTable(int (*hashFcn)(const Object &obj), int (*comp)(const Object&left, const Object&right))
+    {Hash = hashFcn; Compare = comp;}
+    virtual ~HashTable(){}
+    virtual bool contains(const Object & x) const =0;
+    virtual void makeEmpty()=0;
+    virtual bool insert(const Object & x) =0;
+    virtual bool remove(const Object & x) =0;
+    virtual int size() const =0;
+    virtual void displayStatistics() const =0;
+    virtual bool getEntry(const Object & target, Object & returnedItem) const=0;
 };
+
+
+#endif
