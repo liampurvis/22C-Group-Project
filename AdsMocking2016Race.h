7e136299994c1da14cf0c5beffed3c947793963e
oldwhiteoak <purvis.liam@gmail.com>
2015-03-15 13:18:51 -0700

    Update AdsMocking2016Race.h

--- AdsMocking2016Race.h
+++ AdsMocking2016Race.h
@@ -1,151 +1,79 @@
 //
-
 //  AdsMocking2016Race.h
-
 //  Team Project 22C
-
 //
-
 //  Created by Davis Dulin on 2/18/15.
-
 //  Copyright (c) 2015 Liam Purvis. All rights reserved.
-
-//
+//ATTEMPT!!!!!!
 
 #ifndef __Team_Project_22C__AdsMocking2016Race__
-
 #define __Team_Project_22C__AdsMocking2016Race__
 
 #include <stdio.h>
-
 #include <iostream>
-
-#include <string>
 
 using namespace std;
 
 class AdsMocking2016Race {
-
 private:
-
     string name;
-
     string slogan;
-
     string misleadingStat;
-
     double statistic;
-
     
-
 public:
-
     AdsMocking2016Race();
-
-    AdsMocking2016Race(string n, string slo, string m, double stat);
-
-    void setName(string n) {name = n;};
-
-    void setSlogan(string s) {slogan = s;};
-
-    void setMisleadingStat(string m) {misleadingStat = m;};
-
-    void setStatistic(double s) {statistic = s;};
-
-    string getName() const {return name;};
-
-    string getSlogan() const {return slogan;};
-
-    string getMisledingStat() const {return misleadingStat;};
-
-    double getStatistic() const {return statistic;};
-
+    AdsMocking2016Race(string n, string slo, string m, double stat)             //constructor
+    {setName(n); setSlogan(slo); setMisleadingStat(m); setStatistic(stat);}
+    void setName(string n) {name = n;}                                          //setters:
+    void setSlogan(string s) {slogan = s;}
+    void setMisleadingStat(string m) {misleadingStat = m;}
+    void setStatistic(double s) {statistic = s;}
+    string getName() const {return name;}                                       //getters:
+    string getSlogan() const {return slogan;}
+    string getMisledingStat() const {return misleadingStat;}
+    double getStatistic() const {return statistic;}
     
-
-    bool operator != (const AdsMocking2016Race &compAd) const{
-
+    bool operator != (const AdsMocking2016Race &compAd) const{                  //operator overloaders of <,>, !=, <=, & >=:
         if (name != compAd.getName())
-
             return true;
-
         else
-
             return false;
-
     }
-
     
-
     bool operator <= (const AdsMocking2016Race &compAd) const{
-
         if (name <= compAd.getName())
-
-Liam Purvis, Vinitra Swamy, Kartik Vats
-
-CIS 22C - Report #2 - Team #2
-
             return true;
-
         else
-
             return false;
-
     }
-
     
-
     bool operator == (const AdsMocking2016Race &compAd) const{
-
         if (name == compAd.getName())
-
             return true;
-
         else
-
             return false;
-
     }
-
     bool operator >= (const AdsMocking2016Race &compAd) const{
-
         if (name >= compAd.getName())
-
             return true;
-
         else
-
             return false;
-
     }
-
     
-
     bool operator < (const AdsMocking2016Race &compAd) const{
-
         if (name < compAd.getName())
-
             return true;
-
         else
-
             return false;
-
     }
-
     
-
     bool operator > (const AdsMocking2016Race &compAd) const{
-
         if (name > compAd.getName())
-
             return true;
-
         else
-
             return false;
-
     }
-
 };
 
 #endif /* defined(__Team_Project_22C__AdsMocking2016Race__) */
