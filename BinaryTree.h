e9695febb51e57c99a1a3a206bde282b7264cdc9
vinitra <vinitra@users.noreply.github.com>
2015-03-15 13:19:28 -0700

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -8,29 +8,30 @@ template<class ItemType>
 class BinaryTree
 {
 protected:
-    BinaryNode<ItemType>* rootPtr;		// ptr to root node
-    int count;							// number of nodes in tree
+    BinaryNode<ItemType>* rootPtr;      // ptr to root node
+    int count;                          // number of nodes in tree
     
 public:
+
     // "admin" functions
-    BinaryTree()					{rootPtr = 0; count = 0;}
+    BinaryTree()					{ rootPtr = 0; count = 0; }
     BinaryTree(const BinaryTree<ItemType> & tree)
     {
         if(rootPtr != 0)
             clear();
         copyTree(tree.rootPtr);
     }
-    // see assignment
-    virtual ~BinaryTree()				{clear();}// CALL clear()
+    
+    virtual ~BinaryTree()				{ clear(); }              // call clear()
     BinaryTree & operator=(const BinaryTree & sourceTree);
     
     // common functions for all binary trees
-    bool isEmpty() const				{return count == 0;}
-    int size() const					{return count;}
-    void clear(){std::cout<<"\nClearing old tree\n\n"; destroyTree(rootPtr); rootPtr=0; count=0;}
-    void preeOrder(void visit(ItemType &)) const		{_preorder(visit, rootPtr);}
-    void inOrder(void visit(ItemType &)) const		{_inorder(visit, rootPtr);}
-    void postOrder(void visit(ItemType &)) const	{_postorder(visit, rootPtr);}
+    bool isEmpty() const				{ return count == 0; }
+    int size() const					{ return count; }
+    void clear(){std::cout<<"\nClearing Binary Search Tree\n"; destroyTree(rootPtr); rootPtr = 0; count = 0; }
+    void preeOrder(void visit(ItemType &)) const		{ _preorder(visit, rootPtr); }
+    void inOrder(void visit(ItemType &)) const		{ _inorder(visit, rootPtr); }
+    void postOrder(void visit(ItemType &)) const	{ _postorder(visit, rootPtr); }
     
     // abstract functions to be implemented by derived class
     virtual bool insert(const ItemType & newData) = 0;
@@ -38,10 +39,9 @@ public:
     virtual bool getEntry(const ItemType & anEntry, ItemType & returnedItem) const = 0;
     
 private:
+
     // delete all nodes from the tree
     void destroyTree(BinaryNode<ItemType>* nodePtr);
-    
-    
     
     // internal traverse
     void _preorder(void visit(ItemType &), BinaryNode<ItemType>* nodePtr) const;
@@ -61,16 +61,6 @@ template<class ItemType>
 BinaryNode<ItemType>* BinaryTree<ItemType>::copyTree(const BinaryNode<ItemType>* nodePtr)
 {
     BinaryNode<ItemType>* newNodePtr = 0;
-    // FILL IN SEVERAL LINES HERE USING THE ALGORITHM:
-    //    If nodePtr doesn't exist (is 0) Then return 0
-    //
-    //	Assign a new BinaryNode to newNodePtr
-    //	Set TO the newNodePtr's data (item) , the nodePtr's item
-    //	Set TO the newNodePtr's leftPtr,
-    //		a recursive call to copyTree for the nodePtr's leftPtr
-    //     Set TO the newNodePtr's rightPtr,
-    //		a recursive call to copyTree for the nodePtr's rightPtr
-    //	INCREMENT count
     
     if(nodePtr ==0)
         return 0;
@@ -81,7 +71,6 @@ BinaryNode<ItemType>* BinaryTree<ItemType>::copyTree(const BinaryNode<ItemType>*
     
     count++;
     
-    
     return newNodePtr;
 }
 
@@ -91,11 +80,6 @@ BinaryNode<ItemType>* BinaryTree<ItemType>::copyTree(const BinaryNode<ItemType>*
 template<class ItemType>
 void BinaryTree<ItemType>::destroyTree(BinaryNode<ItemType>* nodePtr)
 {
-    //FILL IN SEVERAL LINES HERE, USING THE ALGORITHM:
-    //    if nodePtr doesn't exist (is 0), return
-    //    destroy the left subtree (recursive call to destroyTree)
-    //    destroy the right subtree (recursive call to destroyTree)
-    //    delete nodePtr
     
     if(nodePtr ==0)
         return;
@@ -153,15 +137,11 @@ void BinaryTree<ItemType>::_postorder(void visit(ItemType &), BinaryNode<ItemTyp
 template<class ItemType>
 BinaryTree<ItemType> & BinaryTree<ItemType>::operator=(const BinaryTree<ItemType> & sourceTree)
 {
-    // CALL copyTree and return *this
-    
+
     if(rootPtr != 0) // already filled tree?
         clear(); // clear old tree
     rootPtr = copyTree(sourceTree.rootPtr);
     return *this;
 }
-
-
-
 
 #endif
